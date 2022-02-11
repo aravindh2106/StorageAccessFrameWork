d5cb6dc440b56145d3e552cce0abee7bced60688
aravindh2106 <aravindhan@logicsoft.co.in>
2022-02-11 11:06:26 +0530

    ACTION_OPEN_DOCUMENT implemented

--- app/src/main/java/in/co/logicsoft/storageaccessframework/MainActivity.kt
+++ app/src/main/java/in/co/logicsoft/storageaccessframework/MainActivity.kt
@@ -1,26 +1,26 @@
 package `in`.co.logicsoft.storageaccessframework
 
 import `in`.co.logicsoft.storageaccessframework.databinding.ActivityMainBinding
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import java.io.FileOutputStream
 import java.io.IOException
-import java.io.InputStream
-import java.lang.Exception
-import java.lang.StringBuilder
-import kotlin.jvm.Throws
 
 const val CREATE_DOCUMENT = 100
 const val WRITE_DOCUMENT = 101
 const val READ_DOCUMENT = 102
+const val OPEN_DOCUMENT_TREE = 103
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,6 +34,9 @@ class MainActivity : AppCompatActivity() {
         }
         binding.readBtn.setOnClickListener {
             readFile()
+        }
+        binding.openDirectory.setOnClickListener {
+            openDirectory()
         }
     }
 
@@ -62,16 +65,29 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(createIntent, CREATE_DOCUMENT)
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun openDirectory() {
+        val createIntent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or
+                    Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+        }
+        startActivityForResult(createIntent, OPEN_DOCUMENT_TREE)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         var fileUri: Uri? = null
         if (requestCode == CREATE_DOCUMENT && resultCode == RESULT_OK) {
+            fileUri = data!!.data
             Log.i("STORAGE TAG", "file successfully created")
+            Toast.makeText(this, "$fileUri", Toast.LENGTH_LONG).show()
+
         } else if (requestCode == WRITE_DOCUMENT && resultCode == RESULT_OK) {
             if (data != null) {
                 fileUri = data.data
             }
             writeExternalFile(fileUri!!)
+            Toast.makeText(this, "$fileUri", Toast.LENGTH_LONG).show()
         } else if (requestCode == READ_DOCUMENT && resultCode == RESULT_OK) {
             if (data != null) {
                 fileUri = data.data
@@ -79,10 +95,20 @@ class MainActivity : AppCompatActivity() {
             try {
                 val readData = readExternalFile(fileUri!!)
                 Log.i("STORAGE TAG", "File data::$readData")
-                Toast.makeText(this,"$fileUri",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "$fileUri", Toast.LENGTH_LONG).show()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
+        } else if (requestCode == OPEN_DOCUMENT_TREE && resultCode == RESULT_OK) {
+            if (data != null) {
+                fileUri = data.data
+            }
+
+            contentResolver.takePersistableUriPermission(
+                fileUri!!,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+            Toast.makeText(this, "$fileUri", Toast.LENGTH_LONG).show()
         }
     }
 
